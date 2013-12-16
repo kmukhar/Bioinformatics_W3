69fa5511fcfa24a06e0f57d6ad888e267b282add
kmukhar <kevin.mukhar@gmail.com>
2013-12-16 15:50:41 -0800

    added data files

--- Bioinformatics_W3/src/com/mukhar/MotifEnumeration.java
+++ Bioinformatics_W3/src/com/mukhar/MotifEnumeration.java
@@ -1,5 +1,10 @@
 package com.mukhar;
 
-public class MotifEnumeration {
+import java.util.ArrayList;
 
+public class MotifEnumeration {
+	public ArrayList<String> findMotifs(ArrayList<String> dna, int k, int d) {
+		ArrayList<String> result = new ArrayList<>();
+		return result;
+	}
 }
